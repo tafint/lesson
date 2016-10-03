d8e8c6b20d78188958c32f60eb68361755e6ad08
tafint <nguyenthanhtai.2310@gmail.com>
2016-10-03 09:57:08 +0700

    0.0.3

---
+++ README.md
@@ -0,0 +1,3 @@
+#lesson LAMPART
+Tạo mới folder lesson và clone source code vào folder mới tạo <br />
+Cấu hình database tại config/db.php
--- view/template/header.php
+++ view/template/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Lession 1</title>
+	<title>Lession Training</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="/lesson/public/libs/bootstrap-3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/lesson/public/libs/fancybox/fancybox.css">
